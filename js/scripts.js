407afd4e3231f72689c3f6be688e02fb94117d6f
Kevin Huang <kehuang805@gmail.com>
2021-05-27 10:15:03 -0700

    Reverted nav and update resume

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', event => {
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
-            offset: 15,
+            offset: 74,
         });
     };
 
